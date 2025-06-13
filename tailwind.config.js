2e7efac8440b672b050ae679837c39098b70137b
tosakax2 <tosakax2@gmail.com>
2025-06-13 21:00:58 +0900

    18

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./layouts/**/*.html", "./content/**/*.md"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Yaku Han JP"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
